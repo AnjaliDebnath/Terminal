14d6f7eb88cebf5748891dc78ab0bfa2c79156d1
AnjaliDebnath <anjalidebnath1003@gmail.com>
2024-11-25 12:33:14 +0530

    anjali katai chutiya hai

--- terminal.js
+++ terminal.js
@@ -161,11 +161,11 @@ async function dateFunction() {
    getCharacterByCharacter(str1);
 }
 
-async function errorFunction() {
-  animatedText.innerHTML = '';
-  let language = await langUsed(); // Rename to avoid conflict
-  let strContent = `${language[0]} (${language[1]})! Whoops, this cannot be done. Please use one of the following available commands: help, resume, about, linkedin, random, github, contact, date. ;
-}
+// async function errorFunction() {
+//   animatedText.innerHTML = '';
+//   let language = await langUsed(); // Rename to avoid conflict
+//   let strContent = `${language[0]} (${language[1]})! Whoops, this cannot be done. Please use one of the following available commands: help, resume, about, linkedin, random, github, contact, date. ;
+// }
 
 function checkInputValue(input) {
   command(input);
@@ -196,9 +196,9 @@ function checkInputValue(input) {
     case "date":
         dateFunction();
         break;
-    default:
-      errorFunction();
-      break;
+    // default:
+    //   // errorFunction();
+    //   break;
         
 
 
@@ -213,29 +213,4 @@ function inputCommand(e) {
 help.addEventListener("keypress", inputCommand);
 
 
-
-let minmax = document.querySelector(".button minimize");
-
-    minmax.addEventListener("click", minmaxTab);
-
-    function minmaxTab() {
-      if (terminal.style.height === "100vh") {
-        Terminal.style.height = "50vh";
-        Terminal.style.width = "100%";
-        Terminal.style.maxWidth = "900px";
-      } else {
-        Terminal.style.height = "100vh";
-        Terminal.style.width = "100%";
-        Terminal.style.maxWidth = "100%";
-      }
-    }
-
-    let close = document.querySelector(".close");
-    close.addEventListener("click", closeTab);
-
-    function closeTab() {
-      window.close();
-    }
-  })
-  .catch((error) => console.error("Error fetching the JSON file:", error));
 
